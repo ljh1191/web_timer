bf772398afa3ac9d0b72d9182e4c2b187b0a8c39
leejehwan <dlwpghks1191@gmail.com>
2019-12-19 15:24:20 +0900

    react practice : webtimer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Import component
+import Timer from "./component/Timer";
+
+class App extends Component {
+  constructor(prods){
+    super(prods);
+  }
+
+  render() {
+    return (
+      <div className="App d-flex flex-column container">
+        <div className="Header">
+          <h1>Web Timer App</h1>
+        </div>
+
+        <div className="d-flex flex-fill justify-content-center flex-column">
+          <div className="row">
+            <div className="col-md-6 offset-md-3">
+              <Timer/>
+            </div>
+          </div>
+        </div>
+        <div className="d=flex flex-grow-1"/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/Timer.js
@@ -0,0 +1,124 @@
+import React, { Component } from "react";
+import "./Timer.css";
+
+import * as EasyTimer from "easytimer.js";
+
+class Timer extends Component {
+  constructor(props) {
+    super(props);
+
+    var timer = new EasyTimer();
+
+    this.state = {
+      timer_text: timer.getTimeValues().toString(),
+      timer: timer,
+      timer_state: "stopped"
+    };
+
+    //Bind the functions
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+    this.pauseTimer = this.pauseTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
+    this.logTime = this.logTime.bind(this);
+
+    //Add the listeners
+    timer.addEventListener("secondsUpdated", this.onTimerUpdated.bind(this));
+
+    timer.addEventListener("started", this.onTimerUpdated.bind(this));
+
+    timer.addEventListener("reset", this.onTimerUpdated.bind(this));
+  }
+
+  componentWillUnmount() {
+    if (this.state.timer !== null) {
+      this.state.timer.stop();
+    }
+  }
+
+  onTimerUpdated(e) {
+    this.setState({
+      ...this.state,
+      timer_text: this.state.timer.getTimeValues().toString()
+    });
+  }
+
+  startTimer() {
+    this.state.timer.start();
+
+    this.setState({
+      ...this.state,
+      timer_state: "ticking"
+    });
+  }
+
+  stopTimer() {
+    this.state.timer.stop();
+
+    this.setState({
+      ...this.state,
+      timer_text: "00:00:00",
+      timer_state: "stopped"
+    });
+  }
+
+  pauseTimer() {
+    this.state.timer.pause();
+
+    this.setState({
+      ...this.state,
+      timer_state: "paused"
+    });
+  }
+
+  resetTimer() {
+    this.state.timer.reset();
+
+    this.setState({
+      ...this.state,
+      timer_state: "ticking"
+    });
+  }
+
+  logTime() {
+    console.log(this.state.timer.getTimeValues().toString());
+    this.props.addTime(this.state.timer.getTimeValues());
+  }
+
+  render() {
+    return (
+      <div className="Timer">
+        <div className="timer-text">
+          <h2>{this.state.timer_text}</h2>
+        </div>
+        <div className="timer-buttons text-center">
+          {this.state.timer_state !== "ticking" && (
+            <button onClick={this.startTimer} className="btn btn-success">
+              <i className="fas fa-play" />
+            </button>
+          )}
+
+          {this.state.timer_state === "ticking" && (
+            <button onClick={this.pauseTimer} className="btn btn-warning">
+              <i className="fas fa-pause" />
+            </button>
+          )}
+
+          <button onClick={this.resetTimer} className="btn btn-primary">
+            <i className="fas fa-sync-alt" />
+          </button>
+        </div>
+        <div className="log-button">
+          {/* <button
+            onClick={this.logTime}
+            className="btn btn-block btn-secondary"
+          >
+            Log Time
+          </button> */}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Timer;
